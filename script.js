7a2c3f3658ae3d802900c28a8fb4836a86091316
Vinicius Pascoal <111937912+viniciussk27@users.noreply.github.com>
2024-05-24 01:19:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,30 @@ document.addEventListener('DOMContentLoaded', function() {
   var element = document.getElementById('yourElementId');
   if (element) {
     element.addEventListener('click', function() {
-      // Seu código aqui
+     
     });
   }
 
   const salada = document.getElementById('salada');
   if (salada) {
     salada.addEventListener('click', toggleMenu);
+    salada.addEventListener('touchstart', toggleMenu);
   }
+  
 
-  function toggleMenu() {
+  function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
     if (nav) {
       nav.classList.toggle('active');
+ const active=nav.classList.contains('active')    
+      event.currentTarget.setAttribute('aria-expanded',active)
+      if (active){ event.currentTarget.setAttribute('aria-label','fechar menu');
+ }else {event.currentTarget.setAttribute('aria-label','abrir menu');
+       
+ }
+            
+      
     }
   }
 });
